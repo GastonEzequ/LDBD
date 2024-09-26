20b039383a66cda2c37fa809984dbe1b18fab9f4
Gaston Moreira <161398469+GastonEzequ@users.noreply.github.com>
2024-09-25 21:38:59 -0300

    Add files via upload

---
+++ eliminar_personas.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+<center>	
+<head>
+		<title>Eliminar personas</title>
+	</head>
+	<body>
+	<br>
+	
+		<?php 
+	$conexion=mysqli_connect('localhost','root','','pruebas');
+
+    //hacemos llamado al input de formulario
+    $id = $_POST["pers_id"] ;
+ 
+    $sql="DELETE FROM t_persona WHERE id='$id'";
+			$result=mysqli_query($conexion,$sql);
+            mysqli_close( $conexion );
+	 	?>
+	
+		<form action="bienvenida.html" name="" method="POST">      
+        <tr>
+                <td><label>La persona fue eliminada exitosamente</label></td>
+            </tr>     
+			<tr>
+                <td><input type="submit" value="Volver" /> </td>
+            </tr>
+		</form>
+	</body>
+	</center>
+</html>
--- usuarios.php
+++ usuarios.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html>
+<center>	
+<head>
+		<title>Agregar persona</title>
+	</head>
+	<body>
+	<br>
+	<table border="1" >
+		<tr>
+			<td>Usuario</td>
+			<td>Contraseña</td>
+		</tr>
+
 <?php
 //validamos datos del servidor
 $user = "root";
@@ -13,13 +27,10 @@ if(!$connection)
         {
             echo "No se ha podido conectar con el servidor" . mysqli_connect_error;
         }
-  else
-        {
-            echo "<b><h3>Hemos conectado al servidor</h3></b>" ;
-        }
+ 
         //indicamos el nombre de la base datos
         $datab = "test";
-        //indicamos selecionar ala base datos
+        //indicamos selecionar a la base datos
         $db = mysqli_select_db($connection,$datab);
         //$consulta = "SELECT * FROM tabla where id ='2'"; si queremos que nos muestre solo un registro en especifivo de ID
         $consulta = "SELECT * FROM login";
@@ -28,24 +39,43 @@ if(!$result)
 {
     echo "No se ha podido realizar la consulta";
 }
-echo "<table>";
-echo "<tr>";
-echo "<th><h1>Usuario</th></h1>";
-echo "<th><h1>Contraseña</th></h1>";
-echo "</tr>";
+//echo "<table>";
+//echo "<tr>";
+//echo "<th><h1>Usuario</th></h1>";
+//echo "<th><h1>Contraseña</th></h1>";
+//echo "</tr>";
 
 while ($colum = mysqli_fetch_array($result))
- {
-    echo "<tr>";
+{
+    
+    ?> 
+    <tr>
+    <td><?php echo $colum['usuario'] ?></td>
+    <td><?php echo password_hash($colum['password'],PASSWORD_DEFAULT) ?></td>
+    </tr>
+  <!--echo "<tr>";
     echo "<td><h2>" . $colum['usuario'] . "</td></h2>";
     echo "<td><h2>" . $colum['password'] . "</td></h2>";
     echo "</tr>";
-}
-echo "</table>";
-mysqli_close( $connection );
+    -->
+	<?php 
+			}	
+            mysqli_close( $connection );
+
+
 
    //echo "Fuera " ;
-   echo'<a href="bienvenida.html"> Volver Atrás</a>';
-
+   //echo'<a href="bienvenida.html"> Volver Atrás</a>';
+        
 
 ?>
+</table>
+		<form action="bienvenida.html" name="" method="POST">      
+         
+			<tr>
+                <td><input type="submit" value="Volver" /> </td>
+            </tr>
+		</form>
+	</body>
+	</center>
+</html>
